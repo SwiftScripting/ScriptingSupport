7257e5548a477e1d9be80328c5d3d169011e852e
Paul Calnan <me@paulcalnan.com>
2017-02-23 00:58:09 -0500

    Fixing quote marks

--- Sources/Notification.swift
+++ Sources/Notification.swift
@@ -2,15 +2,15 @@ import Foundation
 
 public func ShowNotification(message: String) {
     Task(launchPath: "/usr/bin/osascript",
-         arguments: ["-e", "'display notification \"\(message)\"'"]).run()
+         arguments: ["-e", "display notification \"\(message)\""]).run()
 }
 
 public func ShowNotification(title: String, message: String) {
     Task(launchPath: "/usr/bin/osascript",
-         arguments: ["-e", "'display notification \"\(message)\" with title \"\(title)\"'"]).run()
+         arguments: ["-e", "display notification \"\(message)\" with title \"\(title)\""]).run()
 }
 
 public func ShowNotification(title: String, subtitle: String, message: String) {
     Task(launchPath: "/usr/bin/osascript",
-         arguments: ["-e", "'display notification \"\(message)\" with title \"\(title)\" subtitle \"\(subtitle)\"'"]).run()
+         arguments: ["-e", "display notification \"\(message)\" with title \"\(title)\" subtitle \"\(subtitle)\""]).run()
 }
